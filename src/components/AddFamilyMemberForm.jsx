00b08a4d3d2bd192ded97bf449d10342de868abe
Axel Vargas <axelvargasvilla1709@outlook.com>
2025-11-18 21:10:14 -0500

    Limite de caracteres en nombre del familiar

--- src/components/AddFamilyMemberForm.jsx
+++ src/components/AddFamilyMemberForm.jsx
@@ -99,7 +99,7 @@ const AddFamilyMemberForm = ({ onAdd, onError }) => {
               onChange={handleChange}
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"
               required
-              maxLength={45}
+              maxLength={30}
             />
           </div>
           <div>
